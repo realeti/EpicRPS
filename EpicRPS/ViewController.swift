fc09642ac6da8b5c575c2bc6a39691bd1ac6b05c
bristol <oro.eto.bristol@gmail.com>
2024-06-10 00:29:41 +0300

    Initial Commit

---
+++ EpicRPS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EpicRPS
+//
+//  Created by Apple M1 on 10.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
