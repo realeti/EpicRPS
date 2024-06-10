0ff9233022ce5f48db9f3df7fc9f70cb2be466ee
bristol <oro.eto.bristol@gmail.com>
2024-06-10 18:12:51 +0300

    first commit

---
+++ EpicRPS/Screens/MainViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MainViewController.swift
+//  EpicRPS
+//
+//  Created by Apple M1 on 10.06.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+        setupConstraints()
+    }
+    
+    // MARK: - Setup UI
+    
+    private func setupUI() {
+        view.backgroundColor = UIColor(resource: .snow)
+        
+        //
+    }
+}
+
+extension MainViewController {
+    
+    // MARK: - Setup Constraints
+    
+    private func setupConstraints() {
+        
+    }
+}
