8485d1d46b539c01d89caebdb437cef1daada170
bristol <oro.eto.bristol@gmail.com>
2024-06-11 22:33:18 +0300

    Added basic game logic

---
+++ EpicRPS/Model/Protocols/GameProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  GameProtocol.swift
+//  EpicRPS
+//
+//  Created by Apple M1 on 11.06.2024.
+//
+
+import Foundation
+
+protocol GameProtocol {
+    func startGame()
+    func startNewRound()
+}
